7021df4543120497c4c2cb0282687ba63f51855b
luki <szymon@storekeeper.nl>
2026-01-10 09:40:56 +0100

    reverted db config setting

--- server/core/config.js
+++ server/core/config.js
@@ -3,77 +3,9 @@ const chalk = require('chalk')
 const cfgHelper = require('../helpers/config')
 const fs = require('fs')
 const path = require('path')
-const url = require('url')
 const yaml = require('js-yaml')
 
 /* global WIKI */
-
-function applyDatabaseUrlEnv(appconfig, DATABASE_URL) {
-  const dbUrl = new url.URL(DATABASE_URL)
-
-  // Extract connection parameters based on database type
-  switch (dbUrl.protocol) {
-    case 'postgres:':
-    case 'postgresql:':
-      appconfig.db.type = 'postgres'
-      break
-    case 'mysql:':
-      appconfig.db.type = 'mysql'
-      break
-    case 'mariadb:':
-      appconfig.db.type = 'mariadb'
-      break
-    case 'mssql:':
-    case 'sqlserver:':
-      appconfig.db.type = 'mssql'
-      break
-    case 'sqlite:':
-      appconfig.db.type = 'sqlite'
-      break
-    default:
-      console.warn(chalk.yellow(`  Unknown database protocol: ${dbUrl.protocol}. Skipping DATABASE_URL apply.`))
-      return false;
-  }
-
-  // SQLite uses a file path instead of host/port/user/pass
-  if (appconfig.db.type === 'sqlite') {
-    if (dbUrl.pathname) {
-      appconfig.db.storage = dbUrl.pathname
-    }
-  } else {
-    // For client-server databases (postgres, mysql, mariadb, mssql)
-    if (dbUrl.hostname) {
-      appconfig.db.host = dbUrl.hostname
-    }
-
-    if (dbUrl.port) {
-      appconfig.db.port = parseInt(dbUrl.port, 10)
-    }
-
-    if (dbUrl.username) {
-      appconfig.db.user = dbUrl.username
-    }
-
-    if (dbUrl.password) {
-      appconfig.db.pass = dbUrl.password
-    }
-
-    if (dbUrl.pathname && dbUrl.pathname.length > 1) {
-      appconfig.db.db = dbUrl.pathname.substring(1) // Remove leading slash
-    }
-  }
-
-  // Parse SSL mode from query parameters
-  const searchParams = new url.URLSearchParams(dbUrl.search)
-  if (searchParams.has('sslmode')) {
-    const sslMode = searchParams.get('sslmode')
-    // SSL is enabled for all modes except 'disable'
-    appconfig.db.ssl = sslMode !== 'disable'
-    console.info(chalk.blue(`  SSL mode: ${sslMode} (ssl: ${appconfig.db.ssl})`))
-  }
-
-  return true;
-}
 
 module.exports = {
   /**
@@ -125,23 +57,6 @@ module.exports = {
     }
 
     const packageInfo = require(path.join(WIKI.ROOTPATH, 'package.json'))
-
-    // Parse DATABASE_URL environment variable
-    if (process.env.DATABASE_URL) {
-      console.info(chalk.blue(`DATABASE_URL is defined. Parsing connection string...`))
-      try {
-        const applied = applyDatabaseUrlEnv(appconfig, process.env.DATABASE_URL);
-        if(applied){
-          console.info(chalk.green.bold(`Database connection parsed successfully`))
-        } else {
-          console.info(chalk.yellow.bold(`DATABASE_URL not applied`))
-        }
-      } catch (err) {
-        console.error(chalk.red.bold(`>>> Failed to parse DATABASE_URL environment variable!`))
-        console.error(err.message)
-        process.exit(1)
-      }
-    }
 
     // Load DB Password from Docker Secret File
     if (process.env.DB_PASS_FILE) {
